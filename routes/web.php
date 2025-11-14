d02b97b254199685cf0a4bda0f09610af6acdb43
Denil Ramos <84945046+DenilRamos@users.noreply.github.com>
2025-11-14 16:00:29 -0500

    chore: instalar laravel/ui para scaffolding de autenticación

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,14 @@
 
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('products',ProductController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
